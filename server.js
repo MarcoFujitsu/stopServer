2dabf3d805cb39c6ace434799a63629042889147
Littel <marco.littel@ts.fujitsu.com>
2019-01-22 12:54:20 +0100

    change unirest to axios

--- server.js
+++ server.js
@@ -2,23 +2,62 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const unirest = require('unirest');
+const axios = require('axios');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.post('/stopinfo', (req, res) => {
-  console.log(req.body);
+var key = process.env.vertrektijdkey;
+axios.defaults.headers.common["Content-Type"] = "application/json";
+axios.defaults.headers.common["X-Vertrektijd-Client-Api-Key"] = key;
 
-  res.send(req.body);
+app.post('/stopinfo', (req, res) => {
+
+  var stopCode = req.body.stopCode;
+
+  var url = "https://api.vertrektijd.info/departures/_stopcode/" + stopCode + "/";
+  
+  axios.get(url)
+  .then(function(response) {
+
+    res.send(response.data);
+  });
+
 });
 
 app.post('/stoplist', (req, res) => {
   console.log(req.body);
   var url = 'https://api.vertrektijd.info/stop/_geo/' + req.body.lat + '/' + req.body.lng + '/0.3';
   
+  axios.get(url)
+  .then(function(response){
+    let buttons = [];
+      for(i=0;i<response.data.length;i++)  {
+          var button = 
+          {
+            title : response.data[i].StopName + " - " + response.data[i].StopCode,
+            type : "value",
+            value : response.data[i].StopCode 
+          }
 
-  var key = process.env.vertrektijdkey;
-  
+          buttons.push(button)
+      }
+      var myResponse = 
+      { replies: [
+      {
+          type : "buttons",
+          content : {
+            title : "Please choose your stop",
+            buttons : buttons
+          }
+
+      }  ]}
+      res.send( myResponse);
+
+
+  });
+
+
   unirest.get(url)
   .headers({'Accept': 'application/json', 'Content-Type': 'application/json', 'X-Vertrektijd-Client-Api-Key' : key })
     .end(function(response){
